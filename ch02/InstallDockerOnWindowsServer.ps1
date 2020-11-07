831c549323e36b012d41aec15d117b942ddb4def
Edwin M Sarmiento <55005497+edwinmsarmiento@users.noreply.github.com>
2020-11-06 21:41:51 -0500

    Update InstallDockerOnWindowsServer.ps1
    Addresses issue defined in https://github.com/OneGet/MicrosoftDockerProvider/issues/78

--- ch02/InstallDockerOnWindowsServer.ps1
+++ ch02/InstallDockerOnWindowsServer.ps1
@@ -1,11 +1,18 @@
 #Install Docker on WIndows Server
 
-Install-WindowsFeature –Name Containers
+Install-WindowsFeature â€“Name Containers
 
 Restart-Computer
 
+#This addresses issue defined in https://github.com/OneGet/MicrosoftDockerProvider/issues/78
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+Register-PSRepository -Default -Verbose
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+#Install the DockerMicrosoft PackageManagement provider
 Install-Module -Name DockerMsftProvider -Force
 
+#Install the Docker package
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
 
 docker version
